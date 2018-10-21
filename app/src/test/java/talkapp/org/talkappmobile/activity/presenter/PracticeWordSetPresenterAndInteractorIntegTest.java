571ecc48da640ffbf904108b5bfa64a6f63c780e
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2018-10-21 15:29:13 +0300

    Added test for practice word set with stops

--- app/src/test/java/talkapp/org/talkappmobile/activity/presenter/PracticeWordSetPresenterAndInteractorIntegTest.java
+++ app/src/test/java/talkapp/org/talkappmobile/activity/presenter/PracticeWordSetPresenterAndInteractorIntegTest.java
@@ -7,10 +7,11 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.io.IOException;
+
+import talkapp.org.talkappmobile.component.AuthSign;
 import talkapp.org.talkappmobile.component.ViewStrategyFactory;
+import talkapp.org.talkappmobile.component.backend.LoginService;
 import talkapp.org.talkappmobile.component.database.PracticeWordSetExerciseRepository;
 import talkapp.org.talkappmobile.model.LoginCredentials;
 import talkapp.org.talkappmobile.model.Sentence;
@@ -34,44 +35,48 @@ public class PracticeWordSetPresenterAndInteractorIntegTest {
     private PracticeWordSetPresenter presenter;
     private PracticeWordSetExerciseRepository exerciseRepository;
     private WordSet wordSet;
+    private PracticeWordSetInteractor interactor;
+    private ViewStrategyFactory viewStrategyFactory;
+    private AuthSign authSign;
+    private LoginService loginService;
 
     @Before
     public void setup() {
         final ClassForInjection injection = new ClassForInjection();
-        String id = "qwe0";
+        viewStrategyFactory = injection.getViewStrategyFactory();
+        interactor = injection.getInteractor();
+        exerciseRepository = injection.getExerciseRepository();
+        authSign = injection.getAuthSign();
+        loginService = injection.getLoginService();
+    }
 
+    private void login(LoginService loginService, final AuthSign authSign) {
+        LoginCredentials credentials = new LoginCredentials();
+        credentials.setEmail("sasha-ne@tut.by");
+        credentials.setPassword("password0");
+        try {
+            authSign.put(loginService.login(credentials).execute().headers().get(AUTHORIZATION_HEADER_KEY));
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private void createPresenter(PracticeWordSetInteractor interactor, ViewStrategyFactory viewStrategyFactory) {
+        String id = "qwe0";
         wordSet = new WordSet();
         wordSet.setId(id);
         wordSet.setWords(asList("age", "anniversary", "birth"));
         wordSet.setTopicId("topicId");
-
-        ViewStrategyFactory viewStrategyFactory = injection.getViewStrategyFactory();
-        PracticeWordSetInteractor interactor = injection.getInteractor();
         PracticeWordSetViewHideNewWordOnlyStrategy newWordOnlyStrategy = viewStrategyFactory.createPracticeWordSetViewHideNewWordOnlyStrategy(view);
         PracticeWordSetViewHideAllStrategy hideAllStrategy = viewStrategyFactory.createPracticeWordSetViewHideAllStrategy(view);
         presenter = new PracticeWordSetPresenter(wordSet, interactor, newWordOnlyStrategy, hideAllStrategy);
-
-        LoginCredentials credentials = new LoginCredentials();
-        credentials.setEmail("sasha-ne@tut.by");
-        credentials.setPassword("password0");
-        injection.getLoginService().login(credentials).enqueue(new Callback<Boolean>() {
-            @Override
-            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                String signature = response.headers().get(AUTHORIZATION_HEADER_KEY);
-                injection.getAuthSign().put(signature);
-            }
-
-            @Override
-            public void onFailure(Call<Boolean> call, Throwable t) {
-
-            }
-        });
-
-        exerciseRepository = injection.getExerciseRepository();
     }
 
     @Test
     public void testPracticeWordSet_completeOneSet() {
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
         presenter.initialise();
         verify(view).setProgress(0);
         reset(view);
@@ -275,6 +280,349 @@ public class PracticeWordSetPresenterAndInteractorIntegTest {
         reset(view);
 
         // sentence 6
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(100);
+        verify(view).showCongratulationMessage();
+        verify(view).closeActivity();
+        verify(view).openAnotherActivity();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+    }
+
+    @Test
+    public void testPracticeWordSet_completeOneSetAndRestartAfterEacheStep() {
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(0);
+        reset(view);
+
+        // sentence 1
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        verify(view).setRightAnswer(matches(ENGLISH));
+        ArgumentCaptor<String> arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length <= 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        String wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        Sentence sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(16);
+        verify(view).setRightAnswer(sentence.getText());
+        verify(view).showNextButton();
+        verify(view).hideCheckButton();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        // sentence 2
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        verify(view).setRightAnswer(matches(ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length <= 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(16);
+        reset(view);
+
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        verify(view).setRightAnswer(matches(ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length <= 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(33);
+        verify(view).setRightAnswer(sentence.getText());
+        verify(view).showNextButton();
+        verify(view).hideCheckButton();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        // sentence 3
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        verify(view).setRightAnswer(matches(ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length <= 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(33);
+        reset(view);
+
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        verify(view).setRightAnswer(matches(ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length <= 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(50);
+        verify(view).setRightAnswer(sentence.getText());
+        verify(view).showNextButton();
+        verify(view).hideCheckButton();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        // sentence 4
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(50);
+        reset(view);
+
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(66);
+        verify(view).setRightAnswer(sentence.getText());
+        verify(view).showNextButton();
+        verify(view).hideCheckButton();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        // sentence 5
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(66);
+        reset(view);
+
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        presenter.checkAnswerButtonClick("");
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageAnswerEmpty();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        wrongWord = "Housd";
+        presenter.checkAnswerButtonClick(wrongWord);
+        verify(view).setEnableCheckButton(false);
+        verify(view).showMessageSpellingOrGrammarError();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).showSpellingOrGrammarErrorPanel(contains(wrongWord));
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        sentence = exerciseRepository.getCurrentSentence(wordSet.getId());
+        presenter.checkAnswerButtonClick(sentence.getText());
+        verify(view).setEnableCheckButton(false);
+        verify(view).setProgress(83);
+        verify(view).setRightAnswer(sentence.getText());
+        verify(view).showNextButton();
+        verify(view).hideCheckButton();
+        verify(view).hideSpellingOrGrammarErrorPanel();
+        verify(view).setEnableCheckButton(true);
+        reset(view);
+
+        // sentence 6
+        presenter.nextButtonClick();
+        verify(view).setEnableNextButton(false);
+        verify(view).hideNextButton();
+        verify(view).showCheckButton();
+        verify(view).setOriginalText(matches(NOT_ENGLISH));
+        arg = ArgumentCaptor.forClass(String.class);
+        verify(view).setRightAnswer(arg.capture());
+        assertTrue(arg.getValue().split(PLACEHOLDER_REGEX).length > 2);
+        verify(view).setAnswerText("");
+        verify(view).setEnableNextButton(true);
+        reset(view);
+
+        createPresenter(interactor, viewStrategyFactory);
+        login(loginService, authSign);
+
+        presenter.initialise();
+        verify(view).setProgress(83);
+        reset(view);
+
         presenter.nextButtonClick();
         verify(view).setEnableNextButton(false);
         verify(view).hideNextButton();
