317e77b166702406e5dd962b18f90292ab88734a
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2018-12-16 16:52:19 +0300

    Revert: Added scoring of sentences into menu (910e189)

--- app/src/main/java/talkapp/org/talkappmobile/activity/PracticeWordSetActivity.java
+++ app/src/main/java/talkapp/org/talkappmobile/activity/PracticeWordSetActivity.java
@@ -1,9 +1,6 @@
 package talkapp.org.talkappmobile.activity;
 
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -40,28 +37,5 @@ public class PracticeWordSetActivity extends BaseActivity {
         PracticeWordSetPagerAdapter sectionsPagerAdapter = new PracticeWordSetPagerAdapter(getSupportFragmentManager(), wordSet, repetitionMode);
 
         viewPager.setAdapter(sectionsPagerAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_practice_word_set_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_scoring) {
-            Toast.makeText(getApplicationContext(), "Scoring doesn't work still", Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
