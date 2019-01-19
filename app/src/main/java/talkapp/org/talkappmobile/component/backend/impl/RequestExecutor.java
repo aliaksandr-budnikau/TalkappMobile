cb3fe891d473dfbb54486965cb64b19e625827ad
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2019-01-19 23:22:58 +0300

    fixed UnknownHostException

--- app/src/main/java/talkapp/org/talkappmobile/component/backend/impl/RequestExecutor.java
+++ app/src/main/java/talkapp/org/talkappmobile/component/backend/impl/RequestExecutor.java
@@ -6,6 +6,7 @@ import org.androidannotations.annotations.EBean;
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -21,7 +22,7 @@ public class RequestExecutor {
     public <T> Response<T> execute(Call<T> call) {
         try {
             return call.execute();
-        } catch (SocketException | SocketTimeoutException e) {
+        } catch (SocketException | UnknownHostException | SocketTimeoutException e) {
             throw new InternetConnectionLostException("Internet connection was lost");
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage(), e);
