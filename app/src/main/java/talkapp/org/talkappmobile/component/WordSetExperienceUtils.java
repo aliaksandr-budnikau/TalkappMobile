5b546f0da87c3beca9787ffab0588a9cae0273f9
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2019-02-22 11:13:56 +0300

    refactoring

--- app/src/main/java/talkapp/org/talkappmobile/component/WordSetExperienceUtils.java
+++ app/src/main/java/talkapp/org/talkappmobile/component/WordSetExperienceUtils.java
@@ -1,13 +1,8 @@
 package talkapp.org.talkappmobile.component;
 
-import talkapp.org.talkappmobile.model.WordSet;
-
 /**
  * @author Budnikau Aliaksandr
  */
 public interface WordSetExperienceUtils {
-    @Deprecated
-    int getProgress(WordSet wordSet, int currentTrainingExperience);
-
     int getProgress(double experience, double maxTrainingExperience);
 }
--- app/src/main/java/talkapp/org/talkappmobile/component/impl/WordSetExperienceUtilsImpl.java
+++ app/src/main/java/talkapp/org/talkappmobile/component/impl/WordSetExperienceUtilsImpl.java
@@ -3,7 +3,6 @@ package talkapp.org.talkappmobile.component.impl;
 import org.androidannotations.annotations.EBean;
 
 import talkapp.org.talkappmobile.component.WordSetExperienceUtils;
-import talkapp.org.talkappmobile.model.WordSet;
 
 import static org.androidannotations.annotations.EBean.Scope.Singleton;
 
@@ -12,12 +11,6 @@ import static org.androidannotations.annotations.EBean.Scope.Singleton;
  */
 @EBean(scope = Singleton)
 public class WordSetExperienceUtilsImpl implements WordSetExperienceUtils {
-
-    @Override
-    public int getProgress(WordSet wordSet, int currentTrainingExperience) {
-        return getProgress((double) currentTrainingExperience, (double) wordSet.getMaxTrainingExperience());
-    }
-
     @Override
     public int getProgress(double experience, double maxTrainingExperience) {
         return (int) (experience / maxTrainingExperience * 100);
