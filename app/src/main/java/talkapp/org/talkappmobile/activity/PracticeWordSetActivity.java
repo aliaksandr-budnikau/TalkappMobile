dd0cc021a273a6903f5e62778ef33571967a596f
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2019-05-21 22:14:43 +0300

    added dialog to prevent closing repetition activity

--- app/src/main/java/talkapp/org/talkappmobile/activity/PracticeWordSetActivity.java
+++ app/src/main/java/talkapp/org/talkappmobile/activity/PracticeWordSetActivity.java
@@ -1,16 +1,23 @@
 package talkapp.org.talkappmobile.activity;
 
+import android.content.DialogInterface;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.KeyDown;
 import org.androidannotations.annotations.ViewById;
 
 import talkapp.org.talkappmobile.R;
 import talkapp.org.talkappmobile.activity.adapter.PracticeWordSetPagerAdapter;
 import talkapp.org.talkappmobile.model.Topic;
 import talkapp.org.talkappmobile.model.WordSet;
+
+import static android.view.KeyEvent.KEYCODE_BACK;
 
 @EActivity(R.layout.activity_practice_word_set)
 public class PracticeWordSetActivity extends BaseActivity {
@@ -37,5 +44,41 @@ public class PracticeWordSetActivity extends BaseActivity {
         PracticeWordSetPagerAdapter sectionsPagerAdapter = new PracticeWordSetPagerAdapter(getSupportFragmentManager(), wordSet, repetitionMode);
 
         viewPager.setAdapter(sectionsPagerAdapter);
+    }
+
+    @KeyDown({KEYCODE_BACK})
+    void enterPressed(KeyEvent keyEvent) {
+        finishActivity();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void finishActivity() {
+        if (!repetitionMode) {
+            PracticeWordSetActivity.this.finish();
+        } else {
+            openDialog();
+        }
+    }
+
+    private void openDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you leaving? Answered words will not be available for now.")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        PracticeWordSetActivity.this.finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null).show();
     }
 }
