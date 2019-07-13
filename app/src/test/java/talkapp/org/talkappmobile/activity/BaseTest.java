df64526e4054b5fbe8492e47467418a3f1fabac7
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2019-07-14 00:33:31 +0300

    Added extra utils methods

--- app/src/test/java/talkapp/org/talkappmobile/activity/BaseTest.java
+++ app/src/test/java/talkapp/org/talkappmobile/activity/BaseTest.java
@@ -1,24 +1,51 @@
 package talkapp.org.talkappmobile.activity;
 
+import android.content.Context;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
 import org.greenrobot.eventbus.EventBus;
 import org.mockito.ArgumentCaptor;
 import org.mockito.verification.VerificationMode;
+import org.powermock.reflect.Whitebox;
+import org.robolectric.RuntimeEnvironment;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import talkapp.org.talkappmobile.dao.DatabaseHelper;
+import talkapp.org.talkappmobile.dao.WordSetDao;
+import talkapp.org.talkappmobile.dao.impl.WordSetDaoImpl;
+import talkapp.org.talkappmobile.mappings.WordSetMapping;
+import talkapp.org.talkappmobile.service.impl.LoggerBean;
+import talkapp.org.talkappmobile.service.impl.ServiceFactoryBean;
 
 import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class BaseTest {
+public class BaseTest {
 
-    <T> T getEM(Class<T> clazz, EventBus eventBus, int times) {
+    private DatabaseHelper databaseHelper;
+    private WordSetDao wordSetDao;
+    private EventBus eventBus;
+
+    protected <T> T getEM(Class<T> clazz, EventBus eventBus, int times) {
         return getValue(clazz, eventBus, times(times));
     }
 
-    <T> T getEM(Class<T> clazz, EventBus eventBus) {
+    protected <T> T getEM(Class<T> clazz, EventBus eventBus) {
+        return getValue(clazz, eventBus, atLeastOnce());
+    }
+
+    protected <T> T getEM(Class<T> clazz, int times) {
+        return getValue(clazz, eventBus, times(times));
+    }
+
+    protected <T> T getEM(Class<T> clazz) {
         return getValue(clazz, eventBus, atLeastOnce());
     }
 
@@ -33,5 +60,38 @@ class BaseTest {
             }
         }
         return null;
+    }
+
+    private <T> T getValue(Class<T> clazz, VerificationMode mode) {
+        return getValue(clazz, eventBus, mode);
+    }
+
+    protected ServiceFactoryBean getServiceFactoryBean() throws SQLException {
+        ServiceFactoryBean serviceFactoryBean = new ServiceFactoryBean();
+        Whitebox.setInternalState(serviceFactoryBean, "logger", new LoggerBean());
+        Whitebox.setInternalState(serviceFactoryBean, "context", mock(Context.class));
+        Whitebox.setInternalState(serviceFactoryBean, "wordSetDao", getWordSetDao());
+        return serviceFactoryBean;
+    }
+
+    protected DatabaseHelper getDatabaseHelper() {
+        if (databaseHelper == null) {
+            databaseHelper = OpenHelperManager.getHelper(RuntimeEnvironment.application, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+
+    protected WordSetDao getWordSetDao() throws SQLException {
+        if (wordSetDao == null) {
+            wordSetDao = new WordSetDaoImpl(getDatabaseHelper().getConnectionSource(), WordSetMapping.class);
+        }
+        return wordSetDao;
+    }
+
+    protected EventBus getEventBus() {
+        if (eventBus == null) {
+            eventBus = mock(EventBus.class);
+        }
+        return eventBus;
     }
 }
