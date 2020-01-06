3a6320cb42b46c4e01dd1deb16ae5c2e86d9e3b6
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2020-01-07 02:21:00 +0300

    refactoring

--- app/src/test/java/talkapp/org/talkappmobile/activity/presenter/TopicsFragmentPresenterAndInteractorIntegTest.java
+++ app/src/test/java/talkapp/org/talkappmobile/activity/presenter/TopicsFragmentPresenterAndInteractorIntegTest.java
@@ -1,55 +1,56 @@
 package talkapp.org.talkappmobile.activity.presenter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.powermock.reflect.Whitebox;
-import talkapp.org.talkappmobile.dao.SentenceDao;
-import talkapp.org.talkappmobile.dao.TopicDao;
-import talkapp.org.talkappmobile.dao.WordTranslationDao;
-import talkapp.org.talkappmobile.model.Topic;
-import talkapp.org.talkappmobile.service.DataServer;
-import talkapp.org.talkappmobile.service.impl.BackendServerFactoryBean;
-import talkapp.org.talkappmobile.service.impl.TopicServiceImpl;
-import talkapp.org.talkappmobile.service.impl.LoggerBean;
-import talkapp.org.talkappmobile.service.impl.RequestExecutor;
-import talkapp.org.talkappmobile.service.impl.ServiceFactoryBean;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import java.util.List;
 
+import talkapp.org.talkappmobile.BuildConfig;
 import talkapp.org.talkappmobile.activity.interactor.TopicsFragmentInteractor;
 import talkapp.org.talkappmobile.activity.view.TopicsFragmentView;
+import talkapp.org.talkappmobile.dao.DatabaseHelper;
+import talkapp.org.talkappmobile.model.Topic;
+import talkapp.org.talkappmobile.service.impl.ServiceFactoryBean;
 
+import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static com.j256.ormlite.android.apptools.OpenHelperManager.getHelper;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = {LOLLIPOP}, packageName = "talkapp.org.talkappmobile.dao.impl")
 public class TopicsFragmentPresenterAndInteractorIntegTest extends PresenterAndInteractorIntegTest {
-    @Mock
     private TopicsFragmentView view;
     private TopicsFragmentInteractor topicsFragmentInteractor;
 
     @Before
     public void setup() {
+        view = mock(TopicsFragmentView.class);
+        ServiceFactoryBean serviceFactory = new ServiceFactoryBean() {
+            private DatabaseHelper helper;
 
-        BackendServerFactoryBean factory = new BackendServerFactoryBean();
-        Whitebox.setInternalState(factory, "logger", new LoggerBean());
-        ServiceFactoryBean mockServiceFactoryBean = mock(ServiceFactoryBean.class);
-        Whitebox.setInternalState(factory, "serviceFactory", mockServiceFactoryBean);
-        Whitebox.setInternalState(factory, "requestExecutor", new RequestExecutor());
-        DataServer server = factory.get();
-        TopicServiceImpl topicService = new TopicServiceImpl(mock(TopicDao.class), server);
+            @Override
+            protected DatabaseHelper databaseHelper() {
+                if (helper != null) {
+                    return helper;
+                }
+                helper = getHelper(RuntimeEnvironment.application, DatabaseHelper.class);
+                return helper;
+            }
+        };
 
-        topicsFragmentInteractor = new TopicsFragmentInteractor(topicService);
+        topicsFragmentInteractor = new TopicsFragmentInteractor(serviceFactory.getTopicService());
     }
 
     @Test
