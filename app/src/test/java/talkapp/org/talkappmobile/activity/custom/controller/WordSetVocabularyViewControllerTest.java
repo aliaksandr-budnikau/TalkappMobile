bb45de85690fa3b3593d384f082d27c6de3d5f7b
alexander.budnikov <aliaksandr.budnikau@gmail.com>
2020-01-07 03:00:28 +0300

    refactoring

--- app/src/test/java/talkapp/org/talkappmobile/activity/custom/controller/WordSetVocabularyViewControllerTest.java
+++ app/src/test/java/talkapp/org/talkappmobile/activity/custom/controller/WordSetVocabularyViewControllerTest.java
@@ -1,79 +1,57 @@
 package talkapp.org.talkappmobile.activity.custom.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
 
 import org.greenrobot.eventbus.EventBus;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.reflect.Whitebox;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import java.sql.SQLException;
 
 import talkapp.org.talkappmobile.BuildConfig;
-import talkapp.org.talkappmobile.DaoHelper;
-import talkapp.org.talkappmobile.dao.TopicDao;
-import talkapp.org.talkappmobile.dao.WordTranslationDao;
+import talkapp.org.talkappmobile.dao.DatabaseHelper;
 import talkapp.org.talkappmobile.events.NewWordSuccessfullySubmittedEM;
 import talkapp.org.talkappmobile.events.NewWordTranslationWasNotFoundEM;
 import talkapp.org.talkappmobile.events.PhraseTranslationInputWasValidatedSuccessfullyEM;
 import talkapp.org.talkappmobile.service.AddingEditingNewWordSetsService;
-import talkapp.org.talkappmobile.service.DataServer;
-import talkapp.org.talkappmobile.service.WordSetService;
 import talkapp.org.talkappmobile.service.impl.AddingEditingNewWordSetsServiceImpl;
-import talkapp.org.talkappmobile.service.impl.BackendServerFactoryBean;
-import talkapp.org.talkappmobile.service.impl.TopicServiceImpl;
-import talkapp.org.talkappmobile.service.impl.LoggerBean;
-import talkapp.org.talkappmobile.service.impl.RequestExecutor;
 import talkapp.org.talkappmobile.service.impl.ServiceFactoryBean;
-import talkapp.org.talkappmobile.service.impl.WordSetServiceImpl;
-import talkapp.org.talkappmobile.service.impl.WordTranslationServiceImpl;
-import talkapp.org.talkappmobile.service.mapper.WordSetMapper;
-import talkapp.org.talkappmobile.service.mapper.WordTranslationMapper;
 
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
+import static com.j256.ormlite.android.apptools.OpenHelperManager.getHelper;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = {LOLLIPOP}, packageName = "talkapp.org.talkappmobile.dao.impl")
 public class WordSetVocabularyViewControllerTest {
-    private WordSetMapper mapper;
-    private WordSetService wordSetService;
     private EventBus eventBus;
     private AddingEditingNewWordSetsService service;
-    private DaoHelper daoHelper;
-    private WordTranslationMapper wordTranslationMapper;
+    private ServiceFactoryBean serviceFactory;
 
     @Before
     public void setUp() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        this.mapper = new WordSetMapper(mapper);
-        this.wordTranslationMapper = new WordTranslationMapper(mapper);
-        daoHelper = new DaoHelper();
-        WordTranslationDao wordTranslationDao = mock(WordTranslationDao.class);
+        serviceFactory = new ServiceFactoryBean() {
+            private DatabaseHelper helper;
 
-        BackendServerFactoryBean factory = new BackendServerFactoryBean();
-        Whitebox.setInternalState(factory, "logger", new LoggerBean());
-        ServiceFactoryBean mockServiceFactoryBean = mock(ServiceFactoryBean.class);
-        WordTranslationServiceImpl wordTranslationService = new WordTranslationServiceImpl(mockServiceFactoryBean.getDataServer(), wordTranslationDao, daoHelper.getWordSetDao(), mapper);
-        when(mockServiceFactoryBean.getWordTranslationService()).thenReturn(wordTranslationService);
-        when(mockServiceFactoryBean.getWordSetExperienceRepository()).thenReturn(wordSetService);
-        Whitebox.setInternalState(factory, "serviceFactory", mockServiceFactoryBean);
-        Whitebox.setInternalState(factory, "requestExecutor", new RequestExecutor());
-        DataServer server = factory.get();
-        TopicServiceImpl localDataService = new TopicServiceImpl(mock(TopicDao.class), server);
-        when(mockServiceFactoryBean.getTopicService()).thenReturn(localDataService);
-        wordSetService = new WordSetServiceImpl(server, daoHelper.getWordSetDao(), daoHelper.getNewWordSetDraftDao(), mapper);
-
+            @Override
+            protected DatabaseHelper databaseHelper() {
+                if (helper != null) {
+                    return helper;
+                }
+                helper = getHelper(RuntimeEnvironment.application, DatabaseHelper.class);
+                return helper;
+            }
+        };
         eventBus = mock(EventBus.class);
-        service = new AddingEditingNewWordSetsServiceImpl(eventBus, server, wordTranslationService);
+        service = new AddingEditingNewWordSetsServiceImpl(eventBus, serviceFactory.getDataServer(), serviceFactory.getWordTranslationService());
     }
 
     @Test
@@ -113,6 +91,6 @@ public class WordSetVocabularyViewControllerTest {
 
     @After
     public void tearDown() {
-        daoHelper.releaseHelper();
+        OpenHelperManager.releaseHelper();
     }
 }
